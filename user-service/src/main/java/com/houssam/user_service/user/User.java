5c7be39a90990eb6c55e34e61a0863a5db5e9d6b
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-03 15:37:11 +0000

    feat: create user entity and user repository

---
+++ user-service/src/main/java/com/houssam/user_service/user/User.java
@@ -0,0 +1,20 @@
+package com.houssam.user_service.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String password;
+
+}
