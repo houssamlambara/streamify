9558fe8c0306bd168d6a6a9ae08713bcbbc787ca
houssam <lambarahoussam@gmail.com>
2026-03-04 11:30:25 +0000

    add enums and model

---
+++ video-service/src/main/java/com/houssam/video_service/model/Video.java
@@ -0,0 +1,51 @@
+package com.houssam.video_service.model;
+
+import com.houssam.video_service.enums.VideoCategory;
+import com.houssam.video_service.enums.VideoType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "videos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false )
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String thumbnailUrl;
+    private String trailerUrl;
+    private Integer duration;
+    private Integer releaseYear;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VideoType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VideoCategory category;
+
+    private Double rating;
+    private String director;
+
+    @Column(columnDefinition = "TEXT")
+    private String cast;
+    private String tmdbId;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
