78b2a632fa1ad906dec55b7f26ac2b14b7d06c5e
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-04 11:12:28 +0000

    feat: implement create user method service

---
+++ user-service/src/main/java/com/houssam/user_service/service/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.houssam.user_service.service;
+
+import com.houssam.user_service.EmailAlreadyExistsException;
+import com.houssam.user_service.dto.UserDto;
+import com.houssam.user_service.entity.User;
+import com.houssam.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    public User createUser(UserDto dto) {
+
+        boolean exists = repository.existsByEmail(dto.getEmail());
+
+        if(exists){
+            throw new EmailAlreadyExistsException("email already exists : "+dto.getEmail());
+        }
+
+        User user = User.builder()
+                .userName(dto.getUserName())
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .build();
+
+        return repository.save(user);
+    }
+
+    @Override
+    public User getUserById(long userId) {
+        return null;
+    }
+
+    @Override
+    public User getAllUser() {
+        return null;
+    }
+
+    @Override
+    public User updateUser(UserDto dto) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(long userId) {
+
+    }
+}
