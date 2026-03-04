ef1a726671b5a03282ff8d03db62af36b235a7dc
houssam <lambarahoussam@gmail.com>
2026-03-04 11:30:58 +0000

    add controller service avec implementation

---
+++ video-service/src/main/java/com/houssam/video_service/service/VideoService.java
@@ -0,0 +1,20 @@
+package com.houssam.video_service.service;
+
+import com.houssam.video_service.dto.VideoResponseDTO;
+import com.houssam.video_service.enums.VideoCategory;
+
+import java.util.List;
+
+public interface VideoService {
+
+    VideoResponseDTO createVideo(VideoResponseDTO request);
+    VideoResponseDTO getVideoById(String id);
+    List<VideoResponseDTO> getAllVideos();
+    VideoResponseDTO updateVideo(String id, VideoResponseDTO request);
+    void deleteVideo(String id);
+    List<VideoResponseDTO> searchByTitle(String title);
+    List<VideoResponseDTO> getByType(String type);
+    List<VideoResponseDTO> getByCategory(String category);
+
+
+}
---
+++ video-service/src/main/java/com/houssam/video_service/service/implementation/VideoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.houssam.video_service.service.implementation;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class VideoServiceImpl {
+}
