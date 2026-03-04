4a62e84bbc8f9cc33d2b90133e86f5432dc1ff8f
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-04 11:01:05 +0000

    feat: create user service interface

--- user-service/src/main/java/com/houssam/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/houssam/user_service/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.houssam.user_service.repository;
 
-import com.houssam.user_service.user.User;
+import com.houssam.user_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
--- user-service/src/main/java/com/houssam/user_service/service/UserService.java
+++ user-service/src/main/java/com/houssam/user_service/service/UserService.java
@@ -1,4 +1,17 @@
 package com.houssam.user_service.service;
 
-public class UserService {
+import com.houssam.user_service.dto.UserDto;
+import com.houssam.user_service.dto.WatchlistDto;
+import com.houssam.user_service.entity.User;
+import com.houssam.user_service.entity.Watchlist;
+
+public interface UserService {
+    User createUser(UserDto dto);
+    User getUserById(long userId);
+    User getAllUser();
+    User updateUser(UserDto dto);
+    void deleteUser(long userId);
+
+    Watchlist addToWatchlist(WatchlistDto dto);
+    void removeFromWatchlist(long watchlistId);
 }
