80c7ab295dc87e2b5160644e7ac93c97d481ab91
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-03-05 14:27:49 +0000

    fix: add params to delete method

--- user-service/src/main/java/com/houssam/user_service/repository/WatchlistRepository.java
+++ user-service/src/main/java/com/houssam/user_service/repository/WatchlistRepository.java
@@ -4,16 +4,14 @@ import com.houssam.user_service.entity.Watchlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface WatchlistRepository extends JpaRepository<Watchlist,Long> {
-    boolean existsByVideoId(String videoId);
+public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
+        boolean existsByVideoId(String videoId);
 
-    @Query("SELECT w FROM Watchlist w WHERE " +
-            "w.userId =:userId AND w.videoId=:videoId")
-    boolean existsByUserIdAndVideoId(Long userId, String videoId);
+        boolean existsByUserIdAndVideoId(Long userId, String videoId);
 
-    @Modifying
-    @Query("DELETE FROM Watchlist w WHERE " +
-            "w.userId =:userId AND w.videoId=:videoId")
-    void deleteByUserIdAndVideoId(Long userId, String videoId);
+        @Modifying
+        @Query("DELETE FROM Watchlist w WHERE w.userId = :userId AND w.videoId = :videoId")
+        void deleteByUserIdAndVideoId(@Param("userId") Long userId, @Param("videoId") String videoId);
 }
